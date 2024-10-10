19c9b5d810563462e4c625dce73267a3f387a805
Cha0i <tom@txcdn.nl>
2024-10-10 16:52:38 +0200

    stole dense runecrafter

--- src/main/java/com/cha0iplugins/cha0iplugins.java
+++ src/main/java/com/cha0iplugins/cha0iplugins.java
@@ -1,20 +1,32 @@
 package com.cha0iplugins;
 
 import com.google.inject.Provides;
+import java.awt.Color;
+import java.util.Objects;
 import javax.inject.Inject;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.GameObject;
 import net.runelite.api.GameState;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemContainer;
+import net.runelite.api.ItemID;
+import net.runelite.api.NullObjectID;
+import net.runelite.api.ObjectID;
+import net.runelite.api.events.GameObjectDespawned;
+import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Cha0i plugins"
+	name = "Cha0i"
 )
 public class cha0iplugins extends Plugin
 {
@@ -24,24 +36,97 @@ public class cha0iplugins extends Plugin
 	@Inject
 	private cha0ipluginsConfig config;
 
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private cha0iOverlay denseRunestoneOverlay;
+
+	private static final int DENSE_RUNESTONE_SOUTH_ID = NullObjectID.NULL_10796;
+	private static final int DENSE_RUNESTONE_NORTH_ID = NullObjectID.NULL_8981;
+
+	@Getter
+	private GameObject denseRunestoneSouth;
+
+	@Getter
+	private GameObject denseRunestoneNorth;
+
+	@Getter
+	private GameObject soulAltar;
+
+	@Getter
+	private GameObject bloodAltar;
+
+	@Getter
+	private Color clickboxBorderColorMinable;
+	@Getter
+	private Color clickboxFillColorMinable;
+	@Getter
+	private Color clickboxBorderHoverColorMinable;
+
+	@Getter
+	private Color clickboxBorderColorDepleted;
+	@Getter
+	private Color clickboxFillColorDepleted;
+	@Getter
+	private Color clickboxBorderHoverColorDepleted;
+
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		overlayManager.add(denseRunestoneOverlay);
+		clickboxBorderColorMinable = config.showDenseRunestoneClickboxAvailable();
+		clickboxFillColorMinable = new Color(clickboxBorderColorMinable.getRed(), clickboxBorderColorMinable.getGreen(), clickboxBorderColorMinable.getBlue(), 50);
+		clickboxBorderHoverColorMinable = clickboxBorderColorMinable.darker();
+
+		clickboxBorderColorDepleted = config.showDenseRunestoneClickboxUnavailable();
+		clickboxFillColorDepleted = new Color(clickboxBorderColorDepleted.getRed(), clickboxBorderColorDepleted.getGreen(), clickboxBorderColorDepleted.getBlue(), 50);
+		clickboxBorderHoverColorDepleted = clickboxBorderColorDepleted.darker();
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (event.getGroup().equals("zomcha0iplugins"))
+		{
+			clickboxBorderColorMinable = config.showDenseRunestoneClickboxAvailable();
+			clickboxFillColorMinable = new Color(clickboxBorderColorMinable.getRed(), clickboxBorderColorMinable.getGreen(), clickboxBorderColorMinable.getBlue(), 50);
+			clickboxBorderHoverColorMinable = clickboxBorderColorMinable.darker();
+
+			clickboxBorderColorDepleted = config.showDenseRunestoneClickboxUnavailable();
+			clickboxFillColorDepleted = new Color(clickboxBorderColorDepleted.getRed(), clickboxBorderColorDepleted.getGreen(), clickboxBorderColorDepleted.getBlue(), 50);
+			clickboxBorderHoverColorDepleted = clickboxBorderColorDepleted.darker();
+		}
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		overlayManager.remove(denseRunestoneOverlay);
+		denseRunestoneNorth = null;
+		denseRunestoneSouth = null;
+		soulAltar = null;
+		bloodAltar = null;
+
+		clickboxFillColorMinable = null;
+		clickboxBorderColorMinable = null;
+		clickboxBorderHoverColorMinable = null;
+		clickboxBorderColorDepleted = null;
+		clickboxFillColorDepleted = null;
+		clickboxBorderHoverColorDepleted = null;
+
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onGameStateChanged(GameStateChanged event)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		GameState gameState = event.getGameState();
+		if (gameState == GameState.LOADING)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			denseRunestoneNorth = null;
+			denseRunestoneSouth = null;
+			bloodAltar = null;
+			soulAltar = null;
 		}
 	}
 
@@ -51,4 +136,52 @@ public class cha0iplugins extends Plugin
 		return configManager.getConfig(cha0ipluginsConfig.class);
 	}
 
+	@Subscribe
+	public void onGameObjectSpawned(GameObjectSpawned event)
+	{
+		GameObject obj = event.getGameObject();
+		int id = obj.getId();
+
+		switch (id)
+		{
+			case DENSE_RUNESTONE_SOUTH_ID:
+				denseRunestoneSouth = obj;
+				break;
+			case DENSE_RUNESTONE_NORTH_ID:
+				denseRunestoneNorth = obj;
+				break;
+			case ObjectID.SOUL_ALTAR:
+				soulAltar = obj;
+				break;
+			case ObjectID.BLOOD_ALTAR:
+				bloodAltar = obj;
+				break;
+		}
+	}
+
+	@Subscribe
+	public void onGameObjectDespawned(GameObjectDespawned event)
+	{
+		switch (event.getGameObject().getId())
+		{
+			case DENSE_RUNESTONE_SOUTH_ID:
+				denseRunestoneSouth = null;
+				break;
+			case DENSE_RUNESTONE_NORTH_ID:
+				denseRunestoneNorth = null;
+				break;
+			case ObjectID.SOUL_ALTAR:
+				soulAltar = null;
+				break;
+			case ObjectID.BLOOD_ALTAR:
+				bloodAltar = null;
+				break;
+		}
+	}
+
+	public boolean hasDarkEssence()
+	{
+		final ItemContainer inventory = client.getItemContainer(InventoryID.INVENTORY);
+		return inventory != null && (inventory.contains(ItemID.DARK_ESSENCE_FRAGMENTS) || inventory.contains(ItemID.DARK_ESSENCE_BLOCK));
+	}
 }
--- src/main/java/com/cha0iplugins/cha0ipluginsConfig.java
+++ src/main/java/com/cha0iplugins/cha0ipluginsConfig.java
@@ -1,5 +1,6 @@
 package com.cha0iplugins;
 
+import java.awt.Color;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
@@ -8,12 +9,57 @@ import net.runelite.client.config.ConfigItem;
 public interface cha0ipluginsConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "We be sideloading like a mf"
+		keyName = "showDenseRunestoneIndicator",
+		name = "Show dense runestone indicator",
+		description = "Configures whether to display an indicator when dense runestone is ready to be mined",
+		position = 1
 	)
-	default String greeting()
+	default boolean showDenseRunestoneIndicator()
 	{
-		return "Hello";
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showDenseRunestoneClickbox",
+		name = "Show dense runestone click box",
+		description = "Configures whether to display a click box when dense runestone is ready to be mined",
+		position = 2
+	)
+	default boolean showDenseRunestoneClickbox()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showDenseRunestoneClickboxAvailable",
+		name = "Available Color",
+		description = "Configures Color of available dense runestone",
+		position = 3
+	)
+	default Color showDenseRunestoneClickboxAvailable()
+	{
+		return Color.GREEN;
+	}
+
+	@ConfigItem(
+		keyName = "showDenseRunestoneClickboxUnavailable",
+		name = "Unavailable Color",
+		description = "Configures color of unavailable dense runestone",
+		position = 4
+	)
+	default Color showDenseRunestoneClickboxUnavailable()
+	{
+		return Color.RED;
+	}
+
+	@ConfigItem(
+			keyName = "highlightAltarClickbox",
+			name = "Highlight Altars",
+			description = "Highlights Blood/Soul Altars when there are dark essence fragments/blocks in inventory",
+			position = 5
+	)
+	default boolean highlightAltarClickbox()
+	{
+		return true;
 	}
 }
